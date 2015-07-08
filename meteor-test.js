02274aa360c078bba479f92b1df8e0b5091c5b8d
Ryan Williams <ryan.blake.williams@gmail.com>
2015-07-08 16:46:29 +0000

    test of unordered limit queries not working

--- meteor-test.js
+++ meteor-test.js
@@ -1,26 +1,58 @@
 
 Test = new Mongo.Collection("test");
 
+var h = null;
+
 if (Meteor.isClient) {
   Tracker.autorun(function() {
-    Meteor.subscribe("test");
+    h = Meteor.subscribe("limit-test");
   });
 
-  Template.table.helpers({
-    num: function() {
-      var r = Test.find().count() + ", " + new Date();
-      console.log(r);
-      return r;
-    },
-    records: function() {
-      return Test.find({}, { sort: { _id: -1 } });
-    }
-  });
 } else if (Meteor.isServer) {
-  Meteor.publish("test", function() {
-    return Test.find({}, {
-      sort: { _id: -1 },
-      //limit: 1000
+  Meteor.publish("limit-test", function() {
+    var limitHandle = Test.find({}, { limit: 100 }).observeChanges({
+      added: function(_id, t) {
+        console.log("limit added: ", _id);
+      },
+      changed: function(_id, fields) {
+        console.log("limit changed: ", _id);
+      },
+      removed: function(_id) {
+        console.log("limit removed: ", _id);
+      }
     });
+
+    var sortLimitHandle = Test.find({}, { sort: { _id: 1 }, limit: 100 }).observeChanges({
+      added: function(_id, t) {
+        console.log("sort-limit added: ", _id);
+      },
+      changed: function(_id, fields) {
+        console.log("sort-limit changed: ", _id);
+      },
+      removed: function(_id) {
+        console.log("sort-limit removed: ", _id);
+      }
+    });
+
+    var noLimitHandle = Test.find({}, {}).observeChanges({
+      added: function(_id, t) {
+        console.log("no-limit added: ", _id);
+      },
+      changed: function(_id, fields) {
+        console.log("no-limit changed: ", _id);
+      },
+      removed: function(_id) {
+        console.log("no-limit removed: ", _id);
+      }
+    });
+
+    this.ready();
+
+    this.onStop(function() {
+      limitHandle.stop();
+      sortLimitHandle.stop();
+      noLimitHandle.stop();
+    });
+
   });
 }
